545771a9e6653a323d57f15e819d7519b51f8f2c
chhaviarora09 <70107327+chhaviarora09@users.noreply.github.com>
2020-10-27 10:00:04 +0530

    Update mobileApi.py

--- mobileApi.py
+++ mobileApi.py
@@ -25,7 +25,7 @@ def removeDir(userId):
             if os.path.isfile(file_path):
                 os.unlink(file_path)
             elif os.path.isdir(file_path): shutil.rmtree(file_path)
-        except Exception, e:
+        except Exception e:
             print e
 
 @app.route("/authenticate",methods=['POST'])
